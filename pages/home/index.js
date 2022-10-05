f010b08566fd691afd3ce3ea3eda733a234c4c59
Gustavo <gucsantostec@gmail.com>
2022-10-05 13:23:47 -0300

    Entrega S3-06 - Renderização ok, falta o darkMode

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,49 @@
+import { products } from "./productsData.js"
 /* Desenvolva sua lógica aqui ... */
+function createHome (data) {       
+        const li = document.createElement("li")
+        const figure = document.createElement("figure")
+        const img = document.createElement("img")
+        const divArtist = document.createElement("div")
+        const nameArtist = document.createElement("p")
+        const year = document.createElement("span")
+        const musicName = document.createElement("h2")
+        const divPrice = document.createElement("div")
+        const price = document.createElement("span")
+        const btnBuy = document.createElement("button")
+
+        li.classList.add("liResult")
+        figure.classList.add("figure")
+        img.classList.add("img")
+        divArtist.classList = "divArtist"
+        nameArtist.classList.add("nameArtist")
+        year.classList.add("yearMusic")
+        musicName.classList.add("nameMusic")
+        divPrice.classList = "divPrice"
+        price.classList.add("price")
+        btnBuy.classList.add("btnBuy")
+        
+        img.src = data.img
+        nameArtist.innerText = data.band
+        year.innerText = data.year
+        musicName.innerText = data.title
+        price.innerText = `R$ ${data.price}`
+        btnBuy.innerText = "Comprar"
+        
+        figure.append(img)
+        divArtist.append(nameArtist,year)
+        divPrice.append(price,btnBuy)
+        li.append(figure,divArtist,musicName,divPrice)
+    
+        return li
+}
+
+export function showMusic (data) {
+    const ul = document.getElementById("ul")
+    ul.innerHTML = ""
+    data.forEach(elem => {
+        const show = createHome(elem)
+        ul.append(show)
+    })
+}
+showMusic(products)
---
+++ scripts/filter.js
@@ -0,0 +1,37 @@
+import { products } from "../pages/home/productsData.js"
+import { categories } from "../pages/home/productsData.js"
+import { showMusic } from "../pages/home/index.js"
+
+
+function filterBtn (produto, categoria) {
+    const btnFilter = document.querySelectorAll(".btnSelection")
+
+    btnFilter.forEach(btn => {
+        btn.addEventListener("click", () =>{
+            const btnText = btn.innerText
+            const categorias = categoria.indexOf(btnText)
+
+            if(btnText === "Todos"){
+                showMusic(products)
+            }else{
+                const produtoFiltrado = produto.filter((filtrado) => filtrado.category === categorias)
+                showMusic(produtoFiltrado)
+
+            }
+        })
+    })
+}
+
+filterBtn(products,categories)
+
+
+function filterPrice (products) {
+    const inputRange = document.querySelector("#inputRange")
+    const spanValue = document.querySelector("#valueInput")
+    inputRange.addEventListener("change", () => {
+        const filter = products.filter(product => product.price <= inputRange.value)
+        spanValue.innerText = `R$ ${inputRange.value},00`   
+        showMusic(filter)
+    })
+}
+filterPrice(products)
